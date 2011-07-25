790d8139c69fd3eb6d09117575679bc10d43b7b1
Rory Zweistra <rory@ryuu.nl>
2011-07-25 14:07:36 +0200

    Initial install of i!8n file

---
+++ WebGUI/lib/WebGUI/i18n/English/Asset_Manify.pm
@@ -0,0 +1,63 @@
+package WebGUI::i18n::English::Asset_Manify;
+use strict;
+
+our $I18N = {
+	'cache timeout' => {
+		message => q|Cache Timeout|,
+		lastUpdated => 0
+	},
+
+	'cache timeout help' => {
+		message => q|Since all users will see this asset the same way, we can cache it for long periods of time to increase performance. How long should we cache it?|,
+		lastUpdated => 1146455970
+	},
+
+	'cacheTimeout' => {
+		message => q|The amount of time this article will be cached.|,
+		lastUpdated => 1164841027
+	},
+
+	'templateId' => {
+		message => q|The Id of the template used to display this Asset.|,
+		lastUpdated => 1164841027
+	},
+
+	'assetName' => {
+		message => q|Manify|,
+		lastUpdated => 1128830974
+	},
+
+	'manify template description' => {
+		message => q|Select a template from the list to layout your Wobject.  Each Wobject
+may only use templates for their own namespace.  For example, Articles
+can only use templates from the "Article" namespace.  Layouts can only
+use templates from the "page" namespace.|,
+		lastUpdated => 1119066250
+	},
+
+	'manify asset template variables title' => {
+		message => q|Article Asset Template Variables|,
+		lastUpdated => 1164841146
+	},
+
+	'manify asset template variables body' => {
+		message => q|Every asset provides a set of variables to most of its
+templates based on the internal asset properties.  Some of these variables may
+be useful, others may not.|,
+		lastUpdated => 1164841201
+	},
+
+	'12' => {
+		message => q|Edit Manify|,
+		lastUpdated => 1031514049
+	},
+
+	'description' => {
+		message => q|The paginated description.|,
+		lastUpdated => 1148960553,
+	},
+
+
+};
+
+1;
